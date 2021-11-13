fe86a9b4c6ff86cfcee3d37f03a959c0e08204cc
pavas <pavel.vas@okwork.io>
2021-11-13 14:33:51 +0300

    add styles for examples

--- src/examples/Main/Main.tsx
+++ src/examples/Main/Main.tsx
@@ -10,12 +10,18 @@ interface IOption extends Partial<Pick<IInputProps, "validators">>{
   description: string;
 }
 
-function timeValidator (time: string) {
+const isValidTime = (time: string): string | undefined => {
   const timeRegex = /^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/ // 0000 - 2359
   const mask = "hh:mm"
 
   if (time.length === mask.length - 1 && !timeRegex.test(time)) {
     return "Invalid time"
+  }
+}
+
+const isNumber = (value: string): string | undefined => {
+  if (!/^\d+$/.test(value)) {
+    return "Value is not a number"
   }
 }
 
@@ -25,36 +31,36 @@ const options: Record<string, IOption> = {
     separators: ["/"],
     name: "date",
     // eslint-disable-next-line max-len
-    description: "<p>just a simple input like date</p><div><strong>mask:</strong> DD/MM/YYYY<br><strong>separators:</strong> [\"/\"]</br>",
+    description: "<b><p>simple input</p></b><div>mask: DD/MM/YYYY<br>separators: [\"/\"]</br>",
   },
   code: {
     mask: "****",
     separators: [" "],
     name: "code",
     // eslint-disable-next-line max-len
-    description: "<p>without separators like code input</p><div><strong>mask:</strong> ****<br><strong>separators:</strong> [\" \"]</br>",
+    description: "<b><p>without separators</p></b><div>mask: ****<br>separators: [\" \"]</br>",
   },
   bankId: {
     mask: "YYYYMMDD-NNNN",
     separators: ["-"],
     name: "bank-id",
     // eslint-disable-next-line max-len
-    description: "<p>mask like a text describing input</p><div><strong>mask:</strong> YYYYMMDD-NNNN<br><strong>separators:</strong> [\"-\"]</div>",
+    description: "<b><p>text mask</p></b><div>mask: YYYYMMDD-NNNN<br>separators: [\"-\"]</div>",
   },
   phone: {
     mask: "+xxx (xx) xxx xx xx",
     separators: ["+", " ", "(", ")"],
     name: "phone",
     // eslint-disable-next-line max-len
-    description: "<p>with multi separators, like phone input</p><div><strong>mask:</strong> +xxx (xx) xxx xx xx<br><strong>separators:</strong> [\"+\", \" \", \"(\", \")\"]</div>",
+    description: "<b><p>multi separators</p></b><div>mask: +xxx (xx) xxx xx xx<br>separators: [\"+\", \" \", \"(\", \")\"]</div>",
   },
   time: {
     mask: "hh-mm",
     separators: ["-"],
     name: "time",
     // eslint-disable-next-line max-len
-    description: "<p>with custom validators</p><div><strong>mask:</strong>hh-mm<br><strong>separators:</strong> [\"-\"]<br><strong>validators:</strong> [timeValidator]</div>",
-    validators: [timeValidator],
+    description: "<b><p>custom validators</p></b><div>mask: hh-mm<br>separators: [\"-\"]<br>validators: <br>[<br>&nbsp;isNumber,<br>&nbsp;isValidTime,<br>]</div>",
+    validators: [isNumber, isValidTime],
   },
 }
 
